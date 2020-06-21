ede4628f47e3ea25129d6a40fa348c01c4acdf0b
Frank Feuerbacher <frank@feuerbacher.us>
2020-06-21 12:40:39 -0500

    Fully uitilize xbmc translated message service

--- resources/lib/common/messages.py
+++ resources/lib/common/messages.py
@@ -6,8 +6,17 @@ Created on Feb 28, 2019
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import xbmc, xbmcaddon
 from .imports import *
-from .constants import Movie
+from .constants import Movie, Constants
+from common.logger import (Logger, LazyLogger, Trace, log_entry_exit)
+
+
+if Constants.INCLUDE_MODULE_PATH_IN_LOGGER:
+    module_logger = LazyLogger.get_addon_module_logger(
+    ).getChild('common.messages')
+else:
+    module_logger = LazyLogger.get_addon_module_logger()
 
 
 class Messages(object):
@@ -33,6 +42,11 @@ class Messages(object):
     GENRE_LABEL = 'Genre: '
     MINUTES_DETAIL = '{} [B]Minutes[/B] - '
     RUNTIME_GENRE = '{} [B]Genre:[/B] {}'
+    TITLE_LABEL = 'Title'
+    DIRECTOR_LABEL = 'Director'
+    WRITER_LABEL = 'Writer'
+    STARS_LABEL = 'Stars'
+    PLOT_LABEL = 'Plot'
 
     GENRE_ACTION = 'Action'
     GENRE_ALEGORY = 'Allegory'
@@ -74,14 +88,83 @@ class Messages(object):
     GENRE_WAR = 'War'
     GENRE_WAR_DOCUMENTARY = 'War Documentary'
     GENRE_WESTERN = 'Western'
+
+    _msg_id_for_name = {
+        TRAILER_EXCEEDS_MAX_PLAY_TIME: 32180,
+        TMDB_LABEL: 32181,
+        ITUNES_LABEL: 32182,
+        MISSING_TITLE: 32183,
+        MISSING_DETAIL: 32184,
+        CAN_NOT_READ_FILE: 32185,
+        CAN_NOT_WRITE_FILE: 32186,
+        NO_TRAILERS_TO_PLAY: 32187,
+        HEADING_INFO: 32188,
+        NO_MOVIE_TO_PLAY: 32189,
+        HEADER_IDLE: 32190,
+        PLAYER_IDLE: 32191,
+        UNLIMITED: 32192,
+        NO_MORE_MOVIE_HISTORY: 32193,
+        PLAYING_PREVIOUS_MOVIE: 32194,
+        GENRE_LABEL: 32195,
+        MINUTES_DETAIL: 32196,
+        RUNTIME_GENRE: 32197,
+        TITLE_LABEL: 32175,
+        DIRECTOR_LABEL: 32176,
+        WRITER_LABEL: 32177,
+        STARS_LABEL: 32178,
+        PLOT_LABEL: 32179,
+        GENRE_ACTION: 32198,
+        GENRE_ALEGORY: 32199,
+        GENRE_ANTHOLOGY: 32200,
+        GENRE_ADVENTURE: 32201,
+        GENRE_ANIMATION: 32202,
+        GENRE_BIOGRAPHY: 32203,
+        GENRE_BLACK_COMEDY: 32204,
+        GENRE_CHILDRENS: 32205,
+        GENRE_COMEDY: 32206,
+        GENRE_COMEDY_DRAMA: 32207,
+        GENRE_CRIME: 32208,
+        GENRE_DOCUMENTARY: 32209,
+        GENRE_DRAMA: 32210,
+        GENRE_EPIC: 32211,
+        GENRE_EXPERIMENTAL: 32212,
+        GENRE_FAMILY: 32213,
+        GENRE_FANTASY: 32214,
+        GENRE_FILM_NOIR: 32215,
+        GENRE_GAME_SHOW: 32216,
+        GENRE_HISTORY: 32217,
+        GENRE_HORROR: 32218,
+        GENRE_MELODRAMA: 32219,
+        GENRE_MUSIC: 32220,
+        GENRE_MUSICAL: 32221,
+        GENRE_MUSICAL_COMEDY: 32222,
+        GENRE_MYSTERY: 32223,
+        GENRE_PERFORMANCE: 32225,
+        GENRE_PRE_CODE: 32226,
+        GENRE_ROMANCE: 32227,
+        GENRE_ROMANCE_COMEDY: 32228,
+        GENRE_SATIRE: 32229,
+        GENRE_SCIENCE_FICTION: 32230,
+        GENRE_SCREWBALL_COMEDY: 32231,
+        GENRE_SWASHBUCKLER: 32232,
+        GENRE_THRILLER: 32233,
+        GENRE_TV_MOVIE: 32234,
+        GENRE_VARIETY: 32235,
+        GENRE_WAR: 32236,
+        GENRE_WAR_DOCUMENTARY: 32237,
+        GENRE_WESTERN: 32238
+    }
+
     _instance = None
+    _debug_dump = False
 
     def __init__(self):
         # type: () -> None
         """
 
         """
-        pass
+        self._logger = module_logger.getChild(self.__class__.__name__)
+
 
     @staticmethod
     def get_instance():
@@ -101,7 +184,8 @@ class Messages(object):
         :param msg_key:
         :return:
         """
-        return msg_key
+
+        return self.get_formatted_msg(msg_key)
 
     def get_formatted_msg(self, msg_key, *args):
         # type: (TextType, Optional[List[TextType]]) -> TextType
@@ -111,7 +195,34 @@ class Messages(object):
         :param args
         :return:
         """
-        return msg_key.format(*args)
+        if not Messages._debug_dump:
+            for msg_number in range(32000, 32238):
+                unformatted_msg = xbmcaddon.Addon(Constants.ADDON_ID).getLocalizedString(
+                    msg_number)
+                if unformatted_msg != "":
+                    self._logger.debug('found msg:', msg_number, unformatted_msg)
+            Messages._debug_dump = True
+
+        msg_id = Messages._msg_id_for_name.get(msg_key, None)
+        unformatted_msg = 'Message not defined'
+        if msg_id is None:
+            if self._logger.isEnabledFor(Logger.ERROR):
+                self._logger.error(
+                    'Can not find msg_id_for_name for message key: {}'.format(msg_key))
+                unformatted_msg = msg_key
+        else:
+            unformatted_msg = xbmcaddon.Addon(Constants.ADDON_ID).getLocalizedString(msg_id)
+            if unformatted_msg == '':
+                unformatted_msg = msg_key
+                if self._logger.isEnabledFor(Logger.ERROR):
+                    self._logger.error(
+                        'Can not find message from strings for message id: {} msg_key: {}'.format(msg_id,
+                                                                                                  msg_key))
+                    unformatted_msg = msg_key
+                    if self._logger.isEnabledFor(Logger.DEBUG):
+                        unformatted_msg += '_dm'
+
+        return unformatted_msg.format(*args)
 
     def get_formated_title(self, movie):
         # type: (Dict[TextType, TextType]) -> TextType
