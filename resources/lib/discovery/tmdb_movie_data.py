682e3b554a8f861ddccff2b78a6d16eacf2402de
Frank Feuerbacher <frank@feuerbacher.us>
2020-09-21 05:42:15 -0500

    Fix bug introduced by change to base type
    movie traversal of discovered_trailers requires use of get_trailers()
    method

--- resources/lib/discovery/tmdb_movie_data.py
+++ resources/lib/discovery/tmdb_movie_data.py
@@ -6,6 +6,8 @@ Created on Feb 10, 2019
 @author: fbacher
 """
 
+from backend.movie_entry_utils import MovieEntryUtils
+from cache.cache_index import CacheIndex
 from common.imports import *
 from common.constants import (Constants, Movie)
 from discovery.abstract_movie_data import AbstractMovieData
@@ -42,8 +44,14 @@ class TMDBMovieData(AbstractMovieData):
         :return:
         """
         movies_with_trailers = 0
-        for movie in self._discovered_trailers:
+        for movie in self._discovered_trailers.get_trailers():
             if movie[Movie.DISCOVERY_STATE] >= Movie.DISCOVERY_COMPLETE:
                 movies_with_trailers += 1
 
         return movies_with_trailers
+
+    def remove_discovered_movie(self, movie):
+        with self._discovered_trailers_lock:
+            super().remove_discovered_movie(movie)
+            tmdb_id = MovieEntryUtils.get_tmdb_id(movie)
+            CacheIndex.remove_unprocessed_movie(int(tmdb_id))
