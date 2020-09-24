000c6c654b91168a2d67e45f90f5e298195f77d7
Frank Feuerbacher <frank@feuerbacher.us>
2020-09-24 14:32:15 -0500

    Added Pattern type hint

--- resources/lib/common/imports.py
+++ resources/lib/common/imports.py
@@ -1,5 +1,5 @@
 from common.development_tools import (Any, Callable, FrozenSet, Optional, Iterable,
                                       List, Dict, Tuple, Sequence, Set, Union,
-                                      MovieType, ClassVar)
+                                      MovieType, Pattern, ClassVar)
 from six import reraise
 
