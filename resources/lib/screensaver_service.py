923084146b9b248c5dd240204078d133f82061a5
Frank Feuerbacher <frank@feuerbacher.us>
2021-04-09 14:34:28 -0500

    Minor changes for clean exit

--- resources/lib/screensaver_service.py
+++ resources/lib/screensaver_service.py
@@ -4,7 +4,7 @@ Created on Feb 12, 2019
 
 @author: Frank Feuerbacher
 """
-
+from common.minimal_monitor import MinimalMonitor
 from common.python_debugger import PythonDebugger
 REMOTE_DEBUG: bool = False
 if REMOTE_DEBUG:
@@ -20,16 +20,20 @@ from common.monitor import Monitor
 from common.logger import (LazyLogger, Trace)
 
 
-module_logger = LazyLogger.get_addon_module_logger(file_path=__file__)
+module_logger: LazyLogger = LazyLogger.get_addon_module_logger(file_path=__file__)
 
 addon = xbmcaddon.Addon()
 do_fullscreen = addon.getSetting('do_fullscreen')
 
 
 def exit_randomtrailers():
-    if PythonDebugger.is_enabled():
-        PythonDebugger.disable()
-    sys.exit(0)
+    try:
+        if PythonDebugger.is_enabled():
+            PythonDebugger.disable()
+        sys.exit(0)
+    except Exception as e:
+        xbmc.log('Exception occurred exiting ' + str(e), xbmc.LOGDEBUG)
+        pass
 
 
 try:
@@ -37,8 +41,8 @@ try:
         if xbmc.Player().isPlaying():
             exit_randomtrailers()
 
-        _monitor = Monitor
-        Trace.enable_all()
+        _monitor = MinimalMonitor
+        # Trace.enable_all()
 
         # Using wait_for_abort to
         # cause Monitor to query Kodi for Abort on the main thread.
@@ -46,7 +50,6 @@ try:
         # sending/receiving events to plugins.
 
         _monitor.throw_exception_if_abort_requested(timeout=0.01)
-        _monitor.set_startup_complete()
 
         message_received = False
         if not message_received:
@@ -59,11 +62,14 @@ try:
             json_text = xbmc.executeJSONRPC(cmd)
             _monitor.throw_exception_if_abort_requested(timeout=0.01)
 
-        _monitor.throw_exception_if_abort_requested(timeout=0.01)
+    MinimalMonitor.abort_requested()
+    while not MinimalMonitor.real_waitForAbort(timeout=0.1):
+        pass
 
 except AbortException:
     pass
 except Exception as e:
+    xbmc.log('Exception occurred exiting2 ' + str(e), xbmc.LOGDEBUG)
     module_logger.exception('')
 finally:
     exit_randomtrailers()
